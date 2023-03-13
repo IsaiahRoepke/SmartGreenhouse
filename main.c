c462e0d0da67c301399b5eb909df1a66e7d6d339
Isaiah Roepke <roepke87@students.rowan.edu>
2023-03-13 12:50:55 -0400

    Initialization functions for sensors

--- main.c
+++ main.c
@@ -4,9 +4,46 @@
 /**
  * main.c
  */
+
+void gasSensorInit();
+void humiditySensorInit();
+void tempSensorInit();
+void soilSensorInit();
+void wifiInit();
+
+
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	gasSensorInit();
+	humiditySensorInit();
+	tempSensorInit();
+	soilSensorInit();
+	wifiInit();
+
+
 	return 0;
+}
+
+
+void gasSensorInit(){
+
+}
+
+void humiditySensorInit(){
+
+}
+
+void tempSensorInit(){
+
+}
+
+void soilSensorInit(){
+
+}
+
+void wifiInit(){
+
 }
