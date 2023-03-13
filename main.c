af0a019a4bb85d0f498b4ba8493c6d627c2643fc
Isaiah Roepke <roepke87@students.rowan.edu>
2023-03-13 13:55:50 -0400

    comment

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
  */
 void resetButtonInit(); //to restart/reset the MSP
 void gasSensorInit(); //takes I2C input
-void humiditySensorInit();
+void humiditySensorInit(); //takes I2C input
 void tempSensorInit(); //uses thermistor as of now. Talk to Trafford to figure out how to implement
 void soilSensorInit(); //uses pulsewidth modulation as the output. Ask trafford what the communication type is. Can't find datasheet
 void wifiInit(); //Initialize wifi module. How is this done on the MSP430?
